468f43e9284f3c601abeb9c7925e0a45a85ff7dd
fidgetlol <matthewgure@gmail.com>
2025-06-21 12:04:41 -0400

    erroneous dep

--- src/global/app.ts
+++ src/global/app.ts
@@ -1,6 +1,4 @@
-import { defineCustomElements } from '@teamhive/pdf-viewer/dist/loader';
 export default async () => {
-    defineCustomElements(window);
 	/**
 	 * The code to be executed should be placed within a default function that is
 	 * exported by the global script. Ensure all of the code in the global script
